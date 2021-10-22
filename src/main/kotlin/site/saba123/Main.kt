97c4bcbc26d67286a5c027e542736c969a9e706e
ribnil <ribnil20@gmail.com>
2021-10-23 03:08:46 +0900

    DataBaseUseCaseの実行

--- src/main/kotlin/site/saba123/Main.kt
+++ src/main/kotlin/site/saba123/Main.kt
@@ -1,9 +1,11 @@
 package site.saba123
 
 import cn.nukkit.plugin.PluginBase
+import site.saba123.usecase.DataBaseUseCase
 
 class Main: PluginBase() {
     override fun onEnable() {
+        DataBaseUseCase.init(dataFolder.path)
         this.server.pluginManager.registerEvents(EventListener(), this)
     }
 }
