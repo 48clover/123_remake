b7f2d5f0636a658b3f25a72febfc43cb7e5cf484
q0v4 <yotu4gure@gmail.com>
2021-10-23 02:48:08 +0900

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // プラグインの追加（参照などのサポート）
 plugins {
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.5.31"
     id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
@@ -10,13 +10,22 @@ version = "1.0-SNAPSHOT"
 // 依存関係の追加
 repositories {
     jcenter()
+    mavenCentral()
     maven {
         url = uri("https://repo.nukkitx.com/main/")
     }
 }
 
 // プロジェクトコンパイル時の設定
+val exposedVersion: String by project
 dependencies {
+    // exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jodatime:$exposedVersion")
+    implementation("org.xerial:sqlite-jdbc:3.30.1")
+    // nukkit
     implementation(kotlin("stdlib"))
     compileOnly("cn.nukkit:nukkit:1.0-SNAPSHOT")
     testCompileOnly("cn.nukkit:nukkit:1.0-SNAPSHOT")
