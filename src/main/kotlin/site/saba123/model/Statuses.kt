e2bfc40bfe661af1f9a4b4bb9532e7d6d2fff4c4
q0v4 <yotu4gure@gmail.com>
2021-10-24 02:07:17 +0900

    rankId -> rank

--- src/main/kotlin/site/saba123/model/Statuses.kt
+++ src/main/kotlin/site/saba123/model/Statuses.kt
@@ -8,7 +8,7 @@ object Statuses: Table() {
     val xuid = varchar("xuid", 20)
     val name = varchar("name", 20)
     val money = integer("money").default(0)
-    val rankId = integer("rank").default(0)
+    val rank = integer("rank").default(0)
     val playTime = integer("play_time").default(0)
 
     override val primaryKey = PrimaryKey(xuid)
@@ -18,7 +18,7 @@ data class Status(
     val xuid: String,
     val name: String,
     val money: Int,
-    val rankId: Int,
+    val rank: Int,
     val playTime: Int
 )
 
@@ -28,7 +28,7 @@ class StatusDTO {
             result[Statuses.xuid],
             result[Statuses.name],
             result[Statuses.money],
-            result[Statuses.rankId],
+            result[Statuses.rank],
             result[Statuses.playTime]
         )
     }
