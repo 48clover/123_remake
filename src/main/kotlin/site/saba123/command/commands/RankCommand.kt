7cd8a9dcd0cc271371f009fdff451c0c989c631b
q0v4 <yotu4gure@gmail.com>
2021-10-27 02:43:00 +0900

    /rank実装

---
+++ src/main/kotlin/site/saba123/command/commands/RankCommand.kt
@@ -0,0 +1,27 @@
+package site.saba123.command.commands
+
+import cn.nukkit.Player
+import cn.nukkit.command.Command
+import cn.nukkit.command.CommandSender
+import site.saba123.model.Rank
+import site.saba123.store.StatusStore
+
+class RankCommand : Command("rank") {
+    init {
+        description = "[管理] プレイヤーの役職を設定します"
+        usage = "/rank [player] [0：観光|1：住民|2：信任|3：管理]"
+    }
+
+    override fun execute(sender: CommandSender, commandLabel: String, args: Array<out String>): Boolean {
+        if (args.size != 2 || sender !is Player) return false
+        val senderName = sender.name
+        val targetName = args[0]
+        val status = StatusStore.getByName(targetName)
+
+        status.rank.update(args[1].toInt())
+        val rank = Rank(args[1].toInt())
+
+        sender.server.broadcastMessage("§a[INFO] §r$senderName §aが §r$targetName §aを §r[${rank.toText()}] にしました！")
+        return true
+    }
+}
