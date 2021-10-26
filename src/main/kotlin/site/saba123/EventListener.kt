e5a0cd7bfc552f74b5fdf139746ab149118405fe
q0v4 <yotu4gure@gmail.com>
2021-10-27 06:56:34 +0900

    Permission追加 Permissionがなければコマンドが弾かれるように

--- src/main/kotlin/site/saba123/EventListener.kt
+++ src/main/kotlin/site/saba123/EventListener.kt
@@ -7,10 +7,13 @@ import cn.nukkit.event.player.PlayerCommandPreprocessEvent
 import cn.nukkit.event.player.PlayerJoinEvent
 import cn.nukkit.event.player.PlayerQuitEvent
 import site.saba123.model.Job
+import site.saba123.model.Permission
 import site.saba123.model.Status
 import site.saba123.repository.JobRepository
+import site.saba123.repository.PermissionRepository
 import site.saba123.repository.StatusRepository
 import site.saba123.store.JobStore
+import site.saba123.store.PermissionStore
 import site.saba123.store.StatusStore
 
 class EventListener: Listener {
@@ -24,21 +27,27 @@ class EventListener: Listener {
         if (player.hasPlayedBefore()) {
             val status: Status = StatusRepository.find(name)!!
             val job: Job = JobRepository.find(name)!!
+            val permission: Permission = PermissionRepository.find(name)!!
 
             // storeに登録
             StatusStore.add(name, status)
             JobStore.add(name, job)
+            PermissionStore.add(name, permission)
 
-            // mcid変更時：status内のnameを更新
+            // mcid変更時：store内のnameを更新
             if (name != status.name.text) {
                 // storeのnameを更新
                 status.name.shiftName(name)
+                job.name.shiftName(name)
+                permission.name.shiftName(name)
             }
         }
 
         // 初参加
         // DBにプレイヤーデータを登録
         StatusRepository.add(xuid, name)
+        JobRepository.add(xuid, name)
+        PermissionRepository.add(xuid, name)
     }
 
 
@@ -48,14 +57,17 @@ class EventListener: Listener {
         val name = player.name
         val status = StatusStore.getByName(name)
         val job = JobStore.getByName(name)
+        val permission = PermissionStore.getByName(name)
 
         // DBの値を更新
         StatusRepository.update(status)
         JobRepository.update(job)
+        PermissionRepository.update(permission)
 
         // Storeからstatusを削除
         StatusStore.delete(name)
         JobStore.delete(name)
+        PermissionStore.delete(name)
     }
 
     @EventHandler
@@ -95,7 +107,12 @@ class EventListener: Listener {
         val name = player.name
         val status = StatusStore.getByName(name)
         val command = event.message.split(" ")[0]
-        if (!status.rank.hasPermission(command)) {
+        val permission: Permission = PermissionStore.getByName(name)
+
+        if (!permission.enabledCommand.contains(command) &&
+            (!status.rank.hasPermission(command) ||
+                    permission.disabledCommand.contains(command))
+        ) {
             player.sendMessage("コマンドの実行権限がありません")
             event.setCancelled()
         }
