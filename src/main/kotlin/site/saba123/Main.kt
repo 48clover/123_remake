d03994e510f04d300b96fddc747e5b48fecbe12e
q0v4 <yotu4gure@gmail.com>
2021-10-28 02:48:55 +0900

    fix

--- src/main/kotlin/site/saba123/Main.kt
+++ src/main/kotlin/site/saba123/Main.kt
@@ -5,7 +5,13 @@ import site.saba123.usecase.DataBaseUseCase
 
 class Main: PluginBase() {
     override fun onEnable() {
+        // config
+        saveDefaultConfig()
+
+        // db
         DataBaseUseCase.init(dataFolder.path)
+
+        // EventListener
         this.server.pluginManager.registerEvents(EventListener(), this)
     }
 }
