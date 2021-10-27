1b71d305a3d98472149f0b1016565d1fbf81ae62
q0v4 <yotu4gure@gmail.com>
2021-10-28 02:57:16 +0900

    実装

--- src/main/kotlin/site/saba123/util/ConfigPool.kt
+++ src/main/kotlin/site/saba123/util/ConfigPool.kt
@@ -1,5 +1,32 @@
 package site.saba123.util
 
+import cn.nukkit.utils.Config
+
 class ConfigPool {
-    companion object
+    companion object {
+        private val configMap: MutableMap<String, Config> = mutableMapOf()
+        fun add(fileName: String, config: Config) {
+            if (isExist(fileName)) return
+            configMap[fileName] = config
+        }
+
+        fun delete(fileName: String) {
+            if (!isExist(fileName)) return
+            configMap.remove(fileName)
+        }
+
+        fun update(fileName: String, config: Config) {
+            if (!isExist(fileName)) return
+            configMap[fileName] = config
+        }
+
+        fun getByName(fileName: String): Config {
+            if (!isExist(fileName)) throw Exception("存在しないキーの代入")
+            return configMap[fileName]!!
+        }
+
+        fun isExist(fileName: String): Boolean {
+            return configMap.contains(fileName)
+        }
+    }
 }
